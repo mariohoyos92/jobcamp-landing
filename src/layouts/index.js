89828e94a9ce0ac9fecbbf4886dc68f0aaa5947d
Mario Hoyos <mariohoyos92@gmail.com>
2018-07-20 14:48:50 -0500

    feat: try to add og tags for fb

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,8 +13,13 @@ const Layout = ({ children, data }) => (
       meta={[{ name: 'keywords', content: 'bootcamp, coding, job' }]}
     >
       <meta name="description" content="Are you a bootcamp grad or a company looking to hire them? This is the place for you."/>
-      <meta property="og:title" content="JobCamp" />
-      <meta property="og:image" content={logoForOgTags} />
+      <meta property="og:title" content={data.site.siteMetadata.title} />
+    <meta property="og:image" content={logoForOgTags} />
+<meta property="og:image:secure_url" content={logoForOgTags} />
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="400" />
+<meta property="og:image:height" content="300" />
+<meta property="og:image:alt" content="JobCamp Logo" />
       <meta charSet="utf-8" />
       <link rel="shortcut icon" href={favicon} />
     </Helmet>

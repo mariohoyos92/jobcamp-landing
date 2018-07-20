9c526e52a663901d42b8d94bc88fd60363eb59b2
Mario Hoyos <mariohoyos92@gmail.com>
2018-07-20 15:19:52 -0500

    trying twitter one more time

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -12,17 +12,20 @@ const Layout = ({ children, data }) => (
       title={data.site.siteMetadata.title}
       meta={[{ name: 'keywords', content: 'bootcamp, coding, job' }]}
     >
-    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:image" content={logoForOgTags} />
+    <meta name="twitter:title" content={data.site.siteMetadata.title}/>
+<meta name="twitter:description" content="Are you a bootcamp grad or a company looking to hire them? This is the place for you."/>
       <meta name="description" content="Are you a bootcamp grad or a company looking to hire them? This is the place for you."/>
       <meta property="og:title" content={data.site.siteMetadata.title} />
       <meta property="og:description"content="Are you a bootcamp grad or a company looking to hire them? This is the place for you."/>
       <meta property="og:type" content="image"/>
     <meta property="og:image" content={logoForOgTags} />
-<meta property="og:image:secure_url" content={logoForOgTags} />
-<meta property="og:image:type" content="image/png" />
-<meta property="og:image:width" content="1608" />
-<meta property="og:image:height" content="644" />
-<meta property="og:image:alt" content="JobCamp Logo" />
+    <meta property="og:image:secure_url" content={logoForOgTags} />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="1608" />
+    <meta property="og:image:height" content="644" />
+      <meta property="og:image:alt" content="JobCamp Logo" />
       <meta charSet="utf-8" />
       <link rel="shortcut icon" href={favicon} />
     </Helmet>
